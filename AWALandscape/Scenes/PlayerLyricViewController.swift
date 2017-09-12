eb9f05be310be39b693ca210bb91a37f5eaaf6e4
touyou <fujiyou1121@gmail.com>
2017-09-12 10:59:03 +0900

    歌詞の自動スクロールに対応

--- AWALandscape/Scenes/PlayerLyricViewController.swift
+++ AWALandscape/Scenes/PlayerLyricViewController.swift
@@ -12,11 +12,17 @@ class PlayerLyricViewController: UIViewController {
 
     let musicManager = MusicManager.shared
     
+    var timer: Timer!
+    
     @IBOutlet weak var lyricTextView: UITextView! {
         
         didSet {
             
             lyricTextView.text = musicManager.lyric ?? "歌詞情報がありません"
+            lyricTextView.setContentOffset(CGPoint(x: 0, y: -100), animated: false)
+            
+            timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(scrolling), userInfo: nil, repeats: true)
+            timer.fire()
         }
     }
     
@@ -25,7 +31,22 @@ class PlayerLyricViewController: UIViewController {
         super.viewDidLoad()
     }
     
+    deinit {
+        
+        if timer.isValid {
+            
+            timer.invalidate()
+        }
+    }
+    
     func prepare() {
+    }
+    
+    func scrolling() {
+        
+        let ratio = musicManager.playPosition / (musicManager.duration == 0 ? 1 : musicManager.duration)
+        let nextY = -100 + 300 * ratio
+        lyricTextView.setContentOffset(CGPoint(x: 0, y: nextY), animated: true)
     }
 }
 
